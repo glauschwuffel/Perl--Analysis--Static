506da8597b24533612c5478b8d49d6fc787843bf
Gregor Goldbach <glauschwuffel@nomaden.org>
2011-12-19 22:23:04 +0100

    class hierarchy for variable declaration has changed, changed class names

--- lib/Perl/Analysis/Static/Question/Lexical/Name.pm
+++ lib/Perl/Analysis/Static/Question/Lexical/Name.pm
@@ -7,7 +7,7 @@ extends 'Perl::Analysis::Static::Question';
 
 sub set_arguments {
 	my ($self, $arguments) = @_;
-	$self->class('Perl::Analysis::Static::Element::Lexical');
+	$self->class('Perl::Analysis::Static::Element::Declaration::Variable');
 	$self->filter(    ['Name'] );
 	$self->arguments( [split(/:/, $arguments)] );
 }
