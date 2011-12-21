c28c214e5a50e73fd0cf5c6818ff9b76df479f11
Gregor Goldbach <glauschwuffel@nomaden.org>
2011-12-21 22:44:39 +0100

    added documentation. fixed bug: list_modules returns reference to hash, not list.

--- lib/Perl/Analysis/Static/ModuleLister.pm
+++ lib/Perl/Analysis/Static/ModuleLister.pm
@@ -1,7 +1,14 @@
 package Perl::Analysis::Static::ModuleLister;
 # ABSTRACT: list all module below a namespace
 
-=head2 DESCRIPTION
+=head1 DESCRIPTION
+
+This class searches for modules of a given prefix in the
+namespace F<Perl::Analysis::Static>.
+
+Is main purpose is to give programs an easy way to show the user
+possible arguments. For example, L<perlanalst> uses it for
+its option C<--list--questions>.
 
 =cut
 
@@ -9,9 +16,24 @@ use Moose;
 
 use Module::List qw(list_modules);
 
+=attr kind
+
+String defining what 'kind' of module you want to find.
+Reasonable values are 'Question', 'Filter', and 'Analysis' since
+there are actually modules in the according namespace.
+
+=cut
+
 has 'kind' => ( is => 'rw', isa => 'Str' );
 
-=head2 list ($kind)
+=method list
+
+Searches for modules that live in the namespace
+F<Perl::Analysis::Static::KIND>. KIND is the attribute C<kind>.
+
+Returns reference to a list with the names of the modules (with
+the Perl::Analysis::Static::KIND removed), or undef if there
+are no modules in this namespace.
 
 =cut
 
@@ -22,13 +44,24 @@ sub list {
     my $stem    = 'Perl::Analysis::Static::' . $self->kind . '::';
     my $modules = list_modules( $stem, { list_modules => 1, recurse => 1 } );
 
+    # if the hash is empty we found nothing
+    return unless keys %$modules;
+
     # remove the stem so the name is readable
     my @result;
-    for my $module (@$modules) {
+    for my $module (keys %$modules) {
         $module =~ s{$stem}{};
         push @result, $module;
     }
     return \@result;
 }
+
+=head1 EXAMPLES
+
+  # find out what questions may be called
+  my $ml = Perl::Analysis::Static::ModuleLister->new(kind => 'Question');
+  my $questions=$ml->list();
+  
+=cut
 
 1;
